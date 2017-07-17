01d2e0da2f0a8cd0ce4e9e9d17448abdd4eb5224
fboukezzoula <fboukezzoula@finaxys.com>
2017-07-17 23:41:54 +0200

    my first rest api restful in go

---
+++ MySimpleAPIRESTFul/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+//On passera une personne à l'API
+func GetPersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+
+}
+
+func GetPeopleEndPoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(people)
+
+}
+
+func CreatePersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+
+}
+
+func DeletePersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+
+	}
+	json.NewEncoder(w).Encode(people)
+
+}
+
+func main() {
+
+	router := mux.NewRouter()
+
+	people = append(people, Person{ID: "1", Firstname: "BOUKEZZOULA", Lastname: "Fouzi", Address: &Address{City: "PARIS", State: "Ile-De-France"}})
+	people = append(people, Person{ID: "2", Firstname: "YOUNG", Lastname: "Angus"})
+
+	router.HandleFunc("/people", GetPeopleEndPoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndPoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndPoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndPoint).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":12345", router))
+
+}
