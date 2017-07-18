bbfb4206722bd25c61348e9197b3edcfc22c0539
BOUKEZZOULA Fouzi <fboukezzoula@finaxys.com>
2017-07-18 13:10:50 +0200

    Update main.go

--- LoadJSONFromFile/main.go
+++ LoadJSONFromFile/main.go
@@ -8,23 +8,21 @@ import (
 type Config struct {
     Database struct {
 		Host string `json:"host"`
-		Port string `json:"port"`	  
+  		Port string  `json:"port"`
 	} `json:"database"`
 	Host string `json:"host"`
 	Port string `json:"port"`
 }
-
 
 func LoadConfiguration(filename string) (Config, error) {
     var config Config
     configFile, err := os.Open(filename)
     defer configFile.Close()
     if err != nil {
-        fmt.Println(err.Error())
- 		return config, err
+       return config, err
     }
     jsonParser := json.NewDecoder(configFile)
-    jsonParser.Decode(&config)
+    err = jsonParser.Decode(&config)
     return config, err
 }
 
